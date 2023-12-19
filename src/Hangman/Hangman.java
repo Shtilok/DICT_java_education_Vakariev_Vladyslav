c0e96d4a5f758f6d59ad6612b49f5c3fea09f7a6
Vakariev <v.i.vakariev@student.khai.edu>
2023-12-19 18:52:58 +0200

    Hangman 2nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("HANGMAN");
         System.out.println("Ласкаво просимо до гри \"Шебениця\"!");
         System.out.println("Гра складається в тому, щоб вгадати слово, відгадуючи по одній літері за раз.");
         System.out.println("Ви готові випробувати свою інтуїцію та словниковий запас? Гра розпочнеться незабаром!");
+        System.out.print("Guess the word: > ");
+        String wordToGuess = "java"; // Задайте слово, яке гравець повинен вгадати
+
+        String playerGuess = scanner.next();
+
+        if (playerGuess.equals(wordToGuess)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
