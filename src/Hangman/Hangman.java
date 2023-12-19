9e9f9139b2d65c03a325229b4ed582be245594b1
Vakariev <v.i.vakariev@student.khai.edu>
2023-12-19 18:51:50 +0200

    Hangman 1st stage

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,8 @@
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("Ласкаво просимо до гри \"Шебениця\"!");
+        System.out.println("Гра складається в тому, щоб вгадати слово, відгадуючи по одній літері за раз.");
+        System.out.println("Ви готові випробувати свою інтуїцію та словниковий запас? Гра розпочнеться незабаром!");
+    }
+}
