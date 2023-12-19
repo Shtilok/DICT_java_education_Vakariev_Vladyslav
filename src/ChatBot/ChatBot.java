5ebebf59d1d21de0e7be868ab4f036cb5ba15f81
Vakariev <v.i.vakariev@student.khai.edu>
2023-12-19 18:43:10 +0200

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        // Ім'я бота (вибране вами)
+        String botName = "MyBot";
+        // Поточний рік
+        int birthYear = 2023;
+
+        // Виводимо привітання з ім'ям та роком створення бота
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+}
