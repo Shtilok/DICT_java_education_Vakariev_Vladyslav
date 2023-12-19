fd666c9be21d37a12124b4efc855ade5d1a0df4f
Vakariev <v.i.vakariev@student.khai.edu>
2023-12-19 19:06:46 +0200

    Hangman 6th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -9,17 +9,17 @@ public class Hangman {
         String[] wordList = {"python", "java", "javascript", "kotlin"};
         String wordToGuess = wordList[random.nextInt(wordList.length)];
         int remainingAttempts = 8;
+        int remainingLives = 8;
         StringBuilder guessedWord = new StringBuilder(wordToGuess.length());
 
-        // Initialize guessedWord with dashes
         for (int i = 0; i < wordToGuess.length(); i++) {
             guessedWord.append('-');
         }
 
         System.out.println("HANGMAN");
-        System.out.print("Guess the word " + guessedWord + ": > ");
+        System.out.println(guessedWord);
 
-        while (remainingAttempts > 0) {
+        while (remainingAttempts > 0 && remainingLives > 0) {
             System.out.print("Input a letter: > ");
             char guess = scanner.next().charAt(0);
 
@@ -34,13 +34,16 @@ public class Hangman {
 
             if (!letterFound) {
                 remainingAttempts--;
-                System.out.println("That letter doesn't appear in the word. Remaining attempts: " + remainingAttempts);
+                remainingLives--;
+                System.out.println("That letter doesn't appear in the word. Remaining attempts: " + remainingAttempts +
+                        ", Remaining lives: " + remainingLives);
             } else {
                 System.out.println("Good guess! " + guessedWord);
             }
 
             if (guessedWord.toString().equals(wordToGuess)) {
                 System.out.println("Congratulations! You guessed the word: " + wordToGuess);
+                System.out.println("You survived!");
                 break;
             }
         }
@@ -48,6 +51,10 @@ public class Hangman {
         if (remainingAttempts == 0) {
             System.out.println("Thanks for playing!");
             System.out.println("You're out of attempts. The word was: " + wordToGuess);
+        }
+
+        if (remainingLives == 0) {
+            System.out.println("You lost!");
         }
     }
 }
