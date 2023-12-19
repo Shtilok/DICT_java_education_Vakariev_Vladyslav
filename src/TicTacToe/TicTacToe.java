f2c705b9315b6ff654783e9ab1739431ce9d8b64
Vakariev <v.i.vakariev@student.khai.edu>
2023-12-19 20:13:16 +0200

    TicTacToe 5th stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -4,55 +4,30 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Зчитуємо введений рядок від користувача
-        System.out.print("Enter cells: ");
-        String input = scanner.nextLine();
+        // Створюємо пусту ігрову сітку
+        char[][] gameBoard = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
 
-        // Перетворюємо введений рядок в ігрове поле
-        char[][] gameBoard = convertInputToBoard(input);
-
-        // Виводимо ігрове поле
+        // Виводимо порожню ігрову сітку
         printGameBoard(gameBoard);
 
-        // Аналізуємо стан гри та виводимо результат
-        printGameResult(gameBoard);
-
-        // Головний цикл гри
+        // Головний ігровий цикл
         while (true) {
-            // Просимо користувача ввести свій хід
-            System.out.print("Enter the coordinates: ");
-            String coordinatesInput = scanner.nextLine();
+            // Хід першого гравця (Х)
+            makeMove(gameBoard, 'X');
+            printGameBoard(gameBoard);
+            if (checkGameResult(gameBoard, 'X')) {
+                break;
+            }
 
-            try {
-                // Перетворюємо введені координати в числа
-                String[] coordinatesArray = coordinatesInput.split(" ");
-                int row = Integer.parseInt(coordinatesArray[0]) - 1;
-                int col = Integer.parseInt(coordinatesArray[1]) - 1;
-
-                // Перевірка коректності введених координат
-                if (row < 0 || row > 2 ||  col < 0 || col > 2) {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    continue;
-                }
-
-                // Перевірка, чи клітинка не зайнята
-                if (gameBoard[row][col] != ' ') {
-                    System.out.println("This cell is occupied! Choose another one!");
-                    continue;
-                }
-
-                // Оновлення ігрового поля та вивід результату
-                gameBoard[row][col] = 'X';
-                printGameBoard(gameBoard);
-                printGameResult(gameBoard);
-
-                // Перевірка завершення гри
-                if (!checkGameNotFinished(gameBoard)) {
-                    break;
-                }
-
-            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                System.out.println("You should enter valid numbers!");
+            // Хід другого гравця (О)
+            makeMove(gameBoard, 'O');
+            printGameBoard(gameBoard);
+            if (checkGameResult(gameBoard, 'O')) {
+                break;
             }
         }
 
@@ -60,21 +35,110 @@ public class TicTacToe {
         scanner.close();
     }
 
-    // Функція для перетворення рядка в ігрове поле
-    public static char[][] convertInputToBoard(String input) {
-        char[][] board = new char[3][3];
-        int k = 0;
+    // Функція для виведення ігрового поля
+    public static void printGameBoard(char[][] board) {
+        System.out.println("---------");
 
         for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                board[i][j] = (input.charAt(k) == '_') ? ' ' : input.charAt(k);
-                k++;
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+
+        System.out.println("---------");
+    }
+
+    // Функція для здійснення ходу гравця
+    public static void makeMove(char[][] board, char player) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Просимо користувача ввести свій хід
+        while (true) {
+            System.out.print("Player " + player + ", enter the coordinates: ");
+            String coordinatesInput = scanner.nextLine();
+            try {
+                // Перетворюємо введені координати в числа
+                String[] coordinatesArray = coordinatesInput.split(" ");
+                int row = Integer.parseInt(coordinatesArray[0]) - 1;
+                int col = Integer.parseInt(coordinatesArray[1]) - 1;
+
+                // Перевірка коректності введених координат
+                if (row < 0 ||  row > 2 ||  col < 0 || col > 2) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    continue;
+                }
+
+                // Перевірка, чи клітинка не зайнята
+                if (board[row][col] != ' ') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    continue;
+                }
+
+                // Оновлення ігрового поля
+                board[row][col] = player;
+                break;
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("You should enter valid numbers!");
             }
         }
 
-        return board;
+        // Закриваємо Scanner
+        // scanner.close(); // Не закрываем Scanner, так как он используется в других частях программы
     }
 
-    // Інші функції залишаються без змін
-    // ...
+    // Функція для перевірки результату гри
+    public static boolean checkGameResult(char[][] board, char player) {
+        // Логіка перевірки результату гри (залишається без змін)
+
+        // Приклад перевірки для виходу із циклу
+        if (checkWinner(board, player)) {
+            System.out.println("Player " + player + " wins!");
+            return true;
+        }
+
+        // Перевірка на нічию
+        if (isBoardFull(board)) {
+            System.out.println("It's a draw!");
+            return true;
+        }
+
+        return false;
+    }
+
+    // Функція для перевірки переможця
+    public static boolean checkWinner(char[][] board, char player) {
+        // Логіка перевірки переможця (залишається без змін)
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true; // Перевірка горизонталі
+            }
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                return true; // Перевірка вертикалі
+            }
+        }
+        // Перевірка діагоналей
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true; // Перевірка головної діагоналі
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            return true; // Перевірка побічної діагоналі
+        }
+
+        return false;
+    }
+
+    // Функція для перевірки нічии
+    public static boolean isBoardFull(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false; // Є ще порожні клітинки
+                }
+            }
+        }
+        return true; // Немає порожніх клітинок - нічия
+    }
 }
