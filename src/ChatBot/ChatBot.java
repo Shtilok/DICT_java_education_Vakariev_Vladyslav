465fd62c174c3459a9614b58362296817089a9aa
Vakariev <v.i.vakariev@student.khai.edu>
2023-12-19 18:44:00 +0200

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         // Ім'я бота (вибране вами)
         String botName = "MyBot";
         // Поточний рік
         int birthYear = 2023;
-
         // Виводимо привітання з ім'ям та роком створення бота
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+
+        // Створюємо об'єкт Scanner для зчитування введення користувача
+        Scanner scanner = new Scanner(System.in);
+
+        // Запитуємо користувача про його ім'я
+        System.out.println("Please, remind me your name.");
+        String userName = scanner.nextLine();
+
+        // Виводимо привітання з ім'ям користувача
+        System.out.println("What a great name you have, " + userName + "!");
+
+        // Закриваємо Scanner
+        scanner.close();
     }
 }
