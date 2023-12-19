6c5a9dcb3b332bd7d6befcbb5298452875e37aee
Vakariev <v.i.vakariev@student.khai.edu>
2023-12-19 18:47:18 +0200

    ChatBot 5th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -26,17 +26,41 @@ public class ChatBot {
         int userAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
         // Виводимо вгаданий вік
         System.out.println("Your age is " + userAge + "; that's a good time to start programming!");
-
         // Просимо користувача ввести число для підрахунку
         System.out.println("Now, let me prove to you that I can count to any number you want!");
         int userNumber = scanner.nextInt();
 
         // Підраховуємо та виводимо числа від 1 до введеного числа
+        // Підраховуємо та виводимо числа від 1 до введеного числа з знаком оклику
         for (int i = 1; i <= userNumber; i++) {
             System.out.println(i + "!");
         }
 
+        // Проводимо тест на програмування
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods in Java?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into small and manageable pieces.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To confuse the programmer.");
+
+        int correctAnswer = 2;
+        int userAnswer;
+
+        do {
+            System.out.print("Enter the number of your answer: ");
+            userAnswer = scanner.nextInt();
+            if (userAnswer != correctAnswer) {
+                System.out.println("Your answer is incorrect. Please try again.");
+            }
+        } while (userAnswer != correctAnswer);
+
+        System.out.println("Congratulations! Your answer is correct.");
+
         // Закриваємо Scanner
         scanner.close();
+
+        // Завершуємо програму
+        System.out.println("Goodbye, have a nice day!");
     }
 }
